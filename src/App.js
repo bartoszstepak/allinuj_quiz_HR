248052f02be226f428c37e83f0935e80a4e12e0b
unknown <bartoszstepak@gmail.com>
2020-11-07 16:01:53 +0100

    websiete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { HashRouter , Route, Switch } from "react-router-dom";
-import Dashboard from './components/dashboard/dashboard';
 import Quiz from './components/quiz/quiz';
-import Impreza from './components/results/impreza';
-import Sport from './components/results/sport';
-import Nauka from './components/results/nauka';
-import Kultura from './components/results/kultura';
 
 import './App.css';
 
@@ -15,12 +10,7 @@ export default class App extends React.Component {
     return (
       <HashRouter>
         <Switch>
-          <Route exact path="/" component={Dashboard} />
-          <Route exact path="/quiz" component={Quiz} />
-          <Route exact path="/result/impreza" component={Impreza} />
-          <Route exact path="/result/sport" component={Sport} />
-          <Route exact path="/result/nauka" component={Nauka} />
-          <Route exact path="/result/kultura" component={Kultura} />
+          <Route exact path="/" component={Quiz} />
         </Switch>
       </HashRouter >
     );
